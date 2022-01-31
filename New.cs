85f2be483b5e26f76bfeafa65bdfc29651a29823
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-01-30 20:06:33 -0500

    created class structure for new logic

---
+++ New.cs
@@ -0,0 +1,23 @@
+namespace playlistimport;
+
+public class New
+{
+    /* create a menu of available columns
+     * ask user to enter what columns they want top see
+     * present the columns asked to the user
+     */
+    public void PrintColumnOptions()
+    {
+        
+    }
+
+    public void UserChoosesColumns()
+    {
+        
+    }
+
+    public void PrintSelectedData()
+    {
+        
+    }
+}
