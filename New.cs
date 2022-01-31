9babc14f998ebbada7b5f5f63e5318dbc9bd88bf
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-01-31 14:55:34 -0500

    added logic for printing data section by section

--- New.cs
+++ New.cs
@@ -30,6 +30,53 @@ public class SongManager
 
     public static void PrintSelectedData(string[] columns)
     {
-        
+        const string filePath = "music.csv";
+        var reader = new StreamReader(filePath);
+        var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Filepath: " + AppDomain.CurrentDomain.BaseDirectory);
+
+        IEnumerable<dynamic> records = csv.GetRecords<dynamic>();
+
+// records = csv.GetRecord<dynamic>();
+
+        foreach (var record in records)
+        {
+            foreach (string column in columns)
+            {
+                switch (column)
+                {
+                    case "name":
+                        Console.Write(record.Name);
+                        break;
+                    case "artist":
+                        Console.Write(record.Name);
+                        break;
+                    case "composer":
+                        Console.Write(record.Name);
+                        break;
+                    case "genre":
+                        Console.Write(record.Name);
+                        break;
+                    case "year":
+                        Console.Write(record.Name);
+                        break;
+                    case "plays":
+                        Console.Write(record.Name);
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
     }
+}
+
+public class Song
+{
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public string Composer { get; set; }
+    public string Genre { get; set; }
+    public DateOnly Year { get; set; }
+    public int Plays { get; set; }
 }
