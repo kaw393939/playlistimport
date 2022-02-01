57e861b57e8097f4b747db80ec4fc0c7bf3c0bd7
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-01-31 19:25:02 -0500

    added comments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using playlistimport;
 
+// print options for the user to choose from
 SongManager.PrintColumnOptions();
 
+// ask the user to choose columns and print their selected data
 SongManager.UserChoosesColumns();
