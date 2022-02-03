715476fcd45b676efbcfc343594175ae1e970635
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-03 13:24:30 -0500

    Created Convertor folder and moved CustomDateYearConvertor.cs to own file

--- Program.cs
+++ Program.cs
@@ -118,22 +118,3 @@ public class CustomIntConverter : DefaultTypeConverter
         }
     }
 }
-//converting for year
-public class CustomDateYearConverter : DefaultTypeConverter
-{
-    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
-    {
-        if (text != "")
-        {
-            var year = int.Parse(text);
-
-            var date = new DateOnly(year, 1, 1);
-            return date;
-        }
-        else
-        {
-            var date = new DateOnly(1, 1, 1);
-            return date;
-        }
-    }
-}
