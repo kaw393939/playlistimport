ded21c5215b75b9999c5e9ebee53034b05e0c302
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-03 13:14:15 -0500

    Moved Song class to own file

--- Program.cs
+++ Program.cs
@@ -137,13 +137,3 @@ public class CustomDateYearConverter : DefaultTypeConverter
         }
     }
 }
-
-public class Song
-{
-    public string Name { get; set; }
-    public string Artist { get; set; }
-    public string Composer { get; set; }
-    public string Genre { get; set; }
-    public DateOnly Year { get; set; }
-    public int Plays { get; set; }
-}
