fb0dc38f03a12a0023daccdbc0e1d69497c4b5c0
anmorales-hub <moralesand2018@gmail.com>
2022-02-04 04:46:20 -0500

    Task: move Song class to own file

--- Program.cs
+++ Program.cs
@@ -106,15 +106,4 @@ public class CustomIntConverter : DefaultTypeConverter
         }
     }
 }
-//converting for year
 
-
-public class Song
-{
-    public string Name { get; set; }
-    public string Artist { get; set; }
-    public string Composer { get; set; }
-    public string Genre { get; set; }
-    public DateOnly Year { get; set; }
-    public int Plays { get; set; }
-}
