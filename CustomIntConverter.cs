ca54ff76c2f30bb50437a4e71b4761bf020d7b30
deniscodes <piroko25>
2022-02-07 16:57:14 -0500

    New File: seperated the CustomIntConverter class into a seperated file

---
+++ CustomIntConverter.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace playlistimport;
+
+public class CustomIntConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (text != "")
+        {
+            return int.Parse(text);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
