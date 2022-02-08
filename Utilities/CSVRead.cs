1ede15e0e72dff2d15080b3dc83985b338b2db85
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-08 16:41:31 -0500

    Created CSVRead.cs file in utilities with generic method FromPath

---
+++ Utilities/CSVRead.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace Utilities;
+
+public class CSVRead
+{
+    public static List<T> FromPath<T, TMap>(string FilePath)
+    {
+        using (var reader = new StreamReader(FilePath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<SongMap>();
+            return csv.GetRecords<T>().ToList();
+        }    
+    }
+}
