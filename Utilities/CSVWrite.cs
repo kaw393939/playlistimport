13f7c6d968ab6b19e545d53bcec87608774748ad
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-08 15:30:35 -0500

    Created CSVWrite.cs Utilities file, generic as well

---
+++ Utilities/CSVWrite.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace Utilities;
+
+public class CSVWrite
+{
+    public static void WriteCSVtoPath<T>(string outputPath, List<T> records)
+    {
+        using (var writer = new StreamWriter(outputPath))
+        using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csvWriter.WriteRecords(records);
+        }
+    }
+}
