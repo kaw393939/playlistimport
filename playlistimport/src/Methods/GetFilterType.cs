5db49d407477a951aa966aca238464c554fb6173
Ido Tanne <idovader@gmail.com>
2022-02-08 23:03:29 -0500

    Implemented project specific methods

---
+++ playlistimport/src/Methods/GetFilterType.cs
@@ -0,0 +1,12 @@
+using utilties;
+
+namespace playlistimport.Methods;
+
+public class GetFilterType
+{
+    static GetFilterType()
+    {
+        Utilities.ConsoleReadLineWithMessage("How would you like to filter the songs?");
+        return ;
+    }
+}
