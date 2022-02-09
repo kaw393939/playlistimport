8f4fdbf6e3562ee6285ae7e4f01003b879fda6a6
Dale <djs93@njit.edu>
2022-02-08 21:02:32 -0500

    Add FilePathOperations class and GetFilePath method

---
+++ Utilities/UserInteraction/FilePathOperations.cs
@@ -0,0 +1,11 @@
+﻿namespace Utilities.UserInteraction;
+
+public class FilePathOperations
+{
+    public static string GetFilePath(string message, string defaultValue)
+    {
+        Console.WriteLine(message);
+        var filePath = Console.ReadLine();
+        return string.IsNullOrEmpty(filePath) ? defaultValue : filePath;
+    }
+}
