ba1abdd1b3185ef63a19c4e76111d8e515b5a12f
Dale <djs93@njit.edu>
2022-02-08 21:03:19 -0500

    Create CsvRead class and generic ReadRecords and ReadDistinctRecords functions

---
+++ Utilities/CsvRead.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace playlistimport.Utilities;
+
+public class CsvRead
+{
+    public static List<T> ReadRecords<T>(string filePath, ClassMap<T> map)
+    {
+        List<T> records;
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap(map);
+            Console.WriteLine("Reading the CSV File\r");
+            records = csv.GetRecords<T>().ToList();
+        }
+
+        Console.WriteLine($"Record Count = {records.Count}\r");
+        Printables.PrintDashSpacer();
+        return records;
+    }
+    
+    public static List<T> ReadDistinctRecords<T>(string filePath, ClassMap<T> map)
+    {
+        List<T> records = ReadRecords<T>(filePath, map);
+        records = ListOperations.RemoveDuplicates(records);
+    
+        Console.WriteLine($"Distinct Record Count = {records.Count}\r");
+        Printables.PrintDashSpacer();
+
+        return records;
+    }
+}
