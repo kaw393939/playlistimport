1b1fddc6f6956b6ef70850a711cfcb5bcdd160d0
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-10 17:14:37 -0500

    Created UserInputs class to hold methods for user interaction, then created GetFilePathUser method

---
+++ UserInputs.cs
@@ -0,0 +1,19 @@
+﻿using Utilities;
+
+namespace playlistimport;
+
+public class UserInputs
+{
+    public static string GetFilePathUser()
+    {
+        Console.WriteLine("Enter The Absolute File Path for the playlist\r");
+        var absoluteFilePath = "";
+        absoluteFilePath = ConsoleRead.ReadConsole();
+        if (absoluteFilePath == "")
+        {
+            absoluteFilePath = "/Users/conno/RiderProjects/playlistimport/data/music.csv";
+        }
+
+        return absoluteFilePath;
+    }
+}
