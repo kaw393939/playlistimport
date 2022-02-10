553d96ffa68aeaece05f353edef9aeac01de0b17
cjl72 <55719709+cjl72@users.noreply.github.com>
2022-02-10 17:19:37 -0500

    Added comments to explain the code for user interaction and reading in csv file

--- Program.cs
+++ Program.cs
@@ -2,15 +2,12 @@
 
 using playlistimport;
 using Utilities;
-//you will need to run "dotnet add package CsvHelper" inside the consoleApp2 Project folder or create the project
-//if you are doing this from scratch or you can create the project with the solution by checking that
-//box when you create it and just add it in the project solution directory
-//put the path to the file you want to import
+
+//User Interaction To Retrieve Data
 var absoluteFilePath = UserInputs.GetFilePathUser();
 var songYear = UserInputs.GetYearUser();
-//here is creating a new list type using a function
 
-
+//Reading In CSV File
 var records = CSVRead.FromPath<Song, SongMap>(absoluteFilePath); 
 
 ConsoleWrite.WriteToConsole($"Record Count = {records.Count}\r");
