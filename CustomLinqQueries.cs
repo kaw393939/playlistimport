9dd51d0f78682c0836ab1e8bb159ce57156a2c94
Ido Tanne <idovader@gmail.com>
2022-02-10 21:16:21 -0500

    changed code style

--- CustomLinqQueries.cs
+++ CustomLinqQueries.cs
@@ -2,15 +2,15 @@ namespace playlistimport;
 
 public class CustomLinqQueries
 {
-    public static List<Song> RemoveDuplicatesBySongName(List<Song> Songs)
+    public static List<Song> RemoveDuplicatesBySongName(IEnumerable<Song> songs)
     {
-        var distinctItems = Songs.GroupBy(x => x.Name).Select(y => y.First());
+        var distinctItems = songs.GroupBy(x => x.Name).Select(y => y.First());
         return distinctItems.ToList();
     }
-    public static List<Song> GetSongsByYear(List<Song> Songs, int songYear)
+    public static List<Song> GetSongsByYear(IEnumerable<Song> songs, int songYear)
     {
-        IEnumerable<Song> songQuery =
-            from song in Songs
+        var songQuery =
+            from song in songs
             orderby song.Plays
             where song.Year == new DateOnly(songYear,1,1)
             select song;
