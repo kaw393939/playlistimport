09c016af74dedd365592dc0b72e8f4f1983d8c33
cjl72 <cjl72@njit.edu>
2022-02-10 19:40:05 -0500

    Added ConsoleWrite for number of songs after year query

--- Program.cs
+++ Program.cs
@@ -10,13 +10,13 @@ var songYear = UserInputs.GetYearUser();
 //Reading In CSV File
 var records = CSVRead.FromPath<Song, SongMap>(absoluteFilePath); 
 
-ConsoleWrite.WriteToConsole($"Record Count = {records.Count}\r");
+ConsoleWrite.WriteToConsole($"Number of CSV File Records = {records.Count}\r");
 ConsoleWrite.WriteToConsole("_____________________________\r");
 //removes duplicates
 var distinctItems = records.GroupBy(x => x.Name).Select(y => y.First());
 //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/
 var songQueryResults = CustomLinqQuery.GetSongsByYr(distinctItems.ToList(), songYear);
-var songCountCount = songQueryResults.Count.ToString();
+ConsoleWrite.WriteToConsole($"Number of Songs from {songYear} = {songQueryResults.Count}\r");
 foreach (Song song in songQueryResults)
 {
     var message = String.Format("{0},{1}, {2}",song.Name,song.Artist, song.Genre);
@@ -24,5 +24,5 @@ foreach (Song song in songQueryResults)
 }
 
 string outPath = "./Output.csv";
-CSVWrite.WriteCSVtoPath<Song>(outPath, songQueryResults);
+CSVWrite.WriteCSVtoPath(outPath, songQueryResults);
 ConsoleWrite.WriteToConsole("Done");
