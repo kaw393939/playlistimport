8724dbbd9568450d076e2555f3d86e956c220b0c
Ido Tanne <idovader@gmail.com>
2022-02-10 21:14:18 -0500

    implemented generic

--- CSVReader.cs
+++ CSVReader.cs
@@ -1,17 +1,18 @@
 using System.Globalization;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace playlistimport;
 
 public class CsvRead
 {
-    public static List<T> FromPath<T,TMap>(string FilePath)
+    public static List<T> FromPath<T,TMap>(string FilePath) where TMap : ClassMap
     {
         
         using (var reader = new StreamReader(FilePath))
         using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
-            csv.Context.RegisterClassMap<SongMap>();
+            csv.Context.RegisterClassMap<TMap>();
             return csv.GetRecords<T>().ToList();
         }
         
