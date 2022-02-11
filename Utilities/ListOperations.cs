1e2dd9ac82a06624a92ec7205a6b25804021c5b0
deniscodes <piroko25>
2022-02-11 10:19:56 -0500

    New File: made  utilities file for checking and removing duplicat songs

---
+++ Utilities/ListOperations.cs
@@ -0,0 +1,10 @@
+﻿namespace playlistimport.Utilities;
+
+public static class ListOperations
+{
+    public static List<T> RemoveDuplicates<T>(List<T> list)
+    {
+        var distinctItems = list.Distinct().ToList();
+        return distinctItems;
+    }
+}
