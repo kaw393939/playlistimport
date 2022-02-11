fc3956708d67402812ec2e0bf15214c5909b5813
deniscodes <piroko25>
2022-02-11 10:38:10 -0500

    Fix: removed last function that was put in another file I forgot to remove in the earlier commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
 using CsvHelper;
 using playlistimport;
 using playlistimport.Utilities;
@@ -7,34 +9,13 @@ using Utilities.UserInteraction;
 //you will need to run "dotnet add package CsvHelper" inside the consoleApp2 Project folder or create the project
 //if you are doing this from scratch or you can create the project with the solution by checking that
 //box when you create it and just add it in the project solution directory
-
 //put the path to the file you want to import
 
-var reader = new StreamReader("../../../data/music.csv");
-var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-var records = csv.GetRecords<Song>();
+var filePath = FilePathOperations.GetFilePath("Enter The Absolute File Path for the playlist\r", 
+    "/Users/kwilliams/RiderProjects/playlistimport/data/music.csv");
 
-foreach (var record in records)
-{
-    Console.WriteLine(record.Name+" || "+record.Artist);
-}
+var records = CsvRead.ReadDistinctRecords(filePath, new SongMap());
 
-public class Song
-{
-    public string Name { get; set; }
-    public string Artist { get; set; }
+var songQuery = CustomQueries.SongByYear(records, FilePathOperations.GetInt("Enter The year\r", 2015));
 
-    public string Composer { get; set; }
-    var filePath = FilePathOperations.GetFilePath("Enter The Absolute File Path for the playlist\r", 
-        "/Users/kwilliams/RiderProjects/playlistimport/data/music.csv");
-
-    public string Genre { get; set; }
-
-    public string Year { get; set; }
-    var records = CsvRead.ReadDistinctRecords(filePath, new SongMap());
-
-    public string Plays { get; set; }
-    var songQuery = CustomQueries.SongByYear(records, FilePathOperations.GetInt("Enter The year\r", 2015));
-
-} 
 CsvWrite.WriteListToCsv(songQuery);
