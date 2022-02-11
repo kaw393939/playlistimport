e0ce5690b94ab92059921b979e3d65659545fb1a
Ido Tanne <idovader@gmail.com>
2022-02-10 21:24:48 -0500

    added user input for artist

--- UserInteractions.cs
+++ UserInteractions.cs
@@ -9,7 +9,7 @@ public class UserInteractions
         ConsoleWrite.WriteToConsole("Enter The Absolute File Path for the playlist\r");
 
         var filePath = ConsoleRead.ReadFromConsole();
-        if (filePath == String.Empty)
+        if (filePath == string.Empty)
         {
             filePath = "/Users/kwilliams/RiderProjects/playlistimport/data/music.csv";
         }
@@ -22,11 +22,24 @@ public class UserInteractions
         ConsoleWrite.WriteToConsole("Enter The year\r");
         
         var songYear = ConsoleRead.ReadFromConsole();
-        if (songYear == String.Empty)
+        if (songYear == string.Empty)
         {
             songYear = "2015";
         }
 
         return int.Parse(songYear);;
+    }
+    
+    public static string GetArtistFromUser()
+    {
+        ConsoleWrite.WriteToConsole("Enter an artist\r");
+        
+        var artist = ConsoleRead.ReadFromConsole();
+        if (artist == string.Empty)
+        {
+            artist = "Bruno Mars";
+        }
+
+        return artist;
     }
 }
