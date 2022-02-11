7a1451ef88efb5430778daa2e4815cbb10338d81
deniscodes <piroko25>
2022-02-11 10:28:45 -0500

    Update: seperated and made seperate functions for int and string getting from console

--- Utilities/UserInteraction/FilePathOperations.cs
+++ Utilities/UserInteraction/FilePathOperations.cs
@@ -8,4 +8,10 @@ public class FilePathOperations
         var filePath = Console.ReadLine();
         return string.IsNullOrEmpty(filePath) ? defaultValue : filePath;
     }
+    public static int GetInt(string message, int defaultValue)
+    {
+        Console.WriteLine(message);
+        var readYear = Console.ReadLine();
+        return !string.IsNullOrEmpty(readYear) ? int.Parse(readYear) : defaultValue;
+    }
 }
