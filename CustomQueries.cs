0ec1de5eaa425297818122a86ded1c2c7a43eccb
deniscodes <piroko25>
2022-02-11 10:15:04 -0500

    New File: sperated and made class for fetching song

---
+++ CustomQueries.cs
@@ -0,0 +1,25 @@
+﻿namespace playlistimport;
+
+public class CustomQueries
+{
+    public static List<Song> SongByYear(List<Song> songs, int year)
+    {
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/
+        IEnumerable<Song> songQuery =
+            from song in songs
+            orderby song.Plays
+            where song.Year.Year == year
+            select song;
+
+        var songQueryResults = songQuery.ToList();
+        var songCountCount = songQueryResults.Count.ToString();
+        Console.WriteLine(songCountCount);
+        foreach (Song song in songQueryResults)
+        {
+            Console.WriteLine("{0},{1}, {2}",song.Name,song.Artist, song.Genre);
+        }
+
+        return songQueryResults;
+    }
+
+}
